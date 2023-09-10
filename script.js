fd289cb91edf12cef5f34a03e6fc884be583bf43
Ayham Shaheen <ayhamjshaheen00@gmail.com>
2023-09-10 20:15:31 +0300

    new Change

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('My Name is \"Ayham Shaheen\" and I am a Developer')
+
+const name1 = 'Ayham';
+let firstName = 'Ayham';
+let lastName = 'Shaheen';
+console.log(name1 , firstName , lastName)
